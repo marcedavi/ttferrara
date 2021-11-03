f5d590e042c3d345f388ae4cea2b11ff44c34d4b
Marcello Davi <davimarcello98@gmail.com>
2021-11-03 17:31:57 +0100

    Phone number

--- page-contatti.php
+++ page-contatti.php
@@ -9,7 +9,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="text-green-600 h-8 w-8 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <p class="font-semibold">+39 1234567890</p>
+                <p class="font-semibold">+39 3498352192</p>
             </div>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-700 h-8 w-8 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
