454c90f67bde1be3dd44e4696601e3e71353f53e
Marcello Davi <davimarcello98@gmail.com>
2021-12-20 16:54:48 +0100

    Fixed contact page

--- page-corsi-e-allenamenti.php
+++ page-corsi-e-allenamenti.php
@@ -6,7 +6,7 @@
     <div class="image-and-text-left">
         <div>
             <h3>Giovani</h3>
-            <p>Per avvicinare bambini e ragazzi a questo bellissimo sport.<br/>Tenuti dal tecnico Maurizio Buzzone il <span class="text-red-800">Lunedì</span>, <span class="text-red-800">Martedì</span> e <span class="text-red-800">Giovedì</span> dalle <span class="text-red-800">17:30</span> alle <span class="text-red-800">19:00</span>.</p>
+            <p>Per avvicinare bambini e ragazzi a questo bellissimo sport.<br/>Tenuti dal tecnico Maurizio Buzzone il <span class="text-red-800">Lunedì</span> e il <span class="text-red-800">Mercoledì</span> dalle <span class="text-red-800">17:00</span> alle <span class="text-red-800">18:30</span> e dalle <span class="text-red-800">18:30</span> alle <span class="text-red-800">20:00</span>.</p>
         </div>
         <?php echo wp_get_attachment_image(get_theme_mod('front_page_courses_1_image_setting'), 'ttferrara_large'); ?>
     </div>
@@ -15,14 +15,14 @@
         <?php echo wp_get_attachment_image(get_theme_mod('front_page_courses_2_image_setting'), 'ttferrara_large'); ?>
         <div>
             <h3>Amatori</h3>
-            <p>Dedicati agli adulti che voglio imparare o rispolverare il Tennis Tavolo. Sempre seguiti dal tecnico Maurizio Buzzone il <span class="text-red-800">Martedì</span> e il <span class="text-red-800">Giovedì</span> dalle <span class="text-red-800">19:15</span> alle <span class="text-red-800">20:45</span>.</p>
+            <p>Dedicati agli adulti che voglio imparare o rispolverare il Tennis Tavolo. Sempre seguiti dal tecnico Maurizio Buzzone il <span class="text-red-800">Lunedì</span> e il <span class="text-red-800">Mercoledì</span> dalle <span class="text-red-800">20:00</span> alle <span class="text-red-800">21:30</span>.</p>
         </div>
     </div>
 
     <div class="image-and-text-left">
         <div>
             <h3>Agonisti</h3>
-            <p>Allenamenti in autonomia per gli atleti che svolgono attività agonistica.<br/>Il <span class="text-red-800">Lunedì</span> dalle <span class="text-red-800">19:15</span> alle <span class="text-red-800">20:45</span>, il <span class="text-red-800">Mercoledì</span> e il <span class="text-red-800">Venerdì</span> dalle <span class="text-red-800">17:30</span> alle <span class="text-red-800">19:00</span> e dalle <span class="text-red-800">19:15</span> alle <span class="text-red-800">20:45</span>.</p>
+            <p>Allenamenti in autonomia per gli atleti che svolgono attività agonistica.<br/>Il <span class="text-red-800">Lunedì</span>, il <span class="text-red-800">Mercoledì</span>, il <span class="text-red-800">Giovedì</span> e il <span class="text-red-800">Venerdì</span> dalle <span class="text-red-800">17:30</span> alle <span class="text-red-800">19:00</span> e dalle <span class="text-red-800">19:15</span> alle <span class="text-red-800">20:45</span>.</p>
         </div>
         <?php echo wp_get_attachment_image(get_theme_mod('front_page_courses_3_image_setting'), 'ttferrara_large'); ?>
     </div>
