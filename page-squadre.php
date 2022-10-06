36487e7f0270c3082d28b2422f047b627018b846
Marcello Davi <davimarcello98@gmail.com>
2022-10-06 15:26:41 +0200

    Aggiornamento squadre

--- page-squadre.php
+++ page-squadre.php
@@ -6,12 +6,12 @@
     <div class="image-and-text-left">
         <div>
             <h3>ASDTT Ferrara A</h3>
-            <h4 class="mb-4">Serie B2 Girone C</h4>
-            <p>Tommaso Seidenari</p>
+            <h4 class="mb-4">Serie C1 Girone F</h4>
             <p>Andrea D'Amore</p>
+            <p>Pietro Andreoli</p>
             <p>Maurizio Buzzone</p>
-            <p>Guido Caravita</p>
-            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=12&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+            <p>Alessandro Andreoli</p>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=37&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_a_image_setting'), 'ttferrara_large'); ?>
     </div>
@@ -20,11 +20,11 @@
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_b_image_setting'), 'ttferrara_large'); ?>
         <div>
             <h3>ASDTT Ferrara B</h3>
-            <h4 class="mb-4">Serie C1 Girone G</h4>
+            <h4 class="mb-4">Serie C1 Girone H</h4>
             <p>Sergio Curarati</p>
+            <p>Luca Antonucci</p>
             <p>Giulio Sani</p>
-            <p>Simone Gallerani</p>
-            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=40&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=39&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
     </div>
 
@@ -32,10 +32,9 @@
         <div>
             <h3>ASDTT Ferrara C</h3>
             <h4 class="mb-4">Serie C2 Girone B</h4>
-            <p>Luca Antonucci</p>
+            <p>Guido Caravita</p>
+            <p>Simone Gallerani</p>
             <p>Enrico Mugellini</p>
-            <p>Silvia Traversi</p>
-            <p>Gianfabrizio Musacchia</p>
             <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=902&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_c_image_setting'), 'ttferrara_large'); ?>
@@ -45,26 +44,24 @@
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_d_image_setting'), 'ttferrara_large'); ?>
         <div>
             <h3>ASDTT Ferrara D</h3>
-            <h4 class="mb-4">Serie D1 Girone B</h4>
-            <p>Alessandro Andreoli</p>
-            <p>Paolo Bianchetti</p>
-            <p>Pietro Andreoli</p>
+            <h4 class="mb-4">Serie C2 Girone A</h4>
             <p>Stefano Zorzan</p>
+            <p>Alessio Giuliani</p>
             <p>Lorenzo Blasi</p>
-            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=904&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+            <p>Claudio Polloni</p>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=901&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
     </div>
 
     <div class="image-and-text-left">
         <div>
             <h3>ASDTT Ferrara E</h3>
-            <h4 class="mb-4">Serie D2 Girone C</h4>
+            <h4 class="mb-4">Serie D1 Girone C</h4>
             <p>Andrea Bonora</p>
-            <p>Claudio Polloni</p>
             <p>Marcello Davi</p>
             <p>Massimo Scalorbi</p>
-            <p>Claudio Sfrisi</p>
-            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=908&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+            <p>Simone Tumiati</p>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=905&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_e_image_setting'), 'ttferrara_large'); ?>
     </div>
@@ -73,13 +70,28 @@
         <?php echo wp_get_attachment_image(get_theme_mod('squadra_f_image_setting'), 'ttferrara_large'); ?>
         <div>
             <h3>ASDTT Ferrara F</h3>
-            <h4 class="mb-4">Serie D2 Girone D</h4>
-            <p>Simone Tumiati</p>
-            <p>Leonardo Lombardi</p>
-            <p>Alessio Giuliani</p>
+            <h4 class="mb-4">Serie D3 Girone D</h4>
+            <p>Aiello Giorgio</p>
+            <p>Stefania Barsotti</p>
             <p>Filippo Azzolina</p>
+            <p>Filippo Pagliarini</p>
+            <p>Claudio Sfrisi</p>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=913&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+        </div>
+    </div>
+
+    <div class="image-and-text-left">
+        <?php echo wp_get_attachment_image(get_theme_mod('squadra_f_image_setting'), 'ttferrara_large'); ?>
+        <div>
+            <h3>ASDTT Ferrara G</h3>
+            <h4 class="mb-4">Serie D3 Girone E</h4>
+            <p>Massimo Fratti</p>
+            <p>Francesco Menini</p>
+            <p>Martino Pace</p>
             <p>Corrado Gangarella</p>
-            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=909&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
+            <p>Marco Franculli</p>
+            <p>Matteo Mordenti</p>
+            <a href="http://portale.fitet.org/risultati/campionati/campionati.php?CAM=914&CONC=0" class="mt-4 link">Classifica, calendario e statistiche</a>
         </div>
     </div>
 </section>
